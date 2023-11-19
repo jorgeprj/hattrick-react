c5f6b3a9e496402fedab8f8c654ff2800014e02d
jorgeprj <jlmp2002@gmail.com>
2023-11-19 19:12:26 -0300

    feat: navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { BrowserRouter as Router } from "react-router-dom"
+import Navbar from "./components/layout/navbar/Navbar.jsx";
+
 function App() {
-  return (
-    <div>
-      Futz
-    </div>
-  )
+	return (
+		<div>
+			<Router>
+				<Navbar />
+			</Router>
+		</div>
+	)
 }
 
 export default App
---
+++ src/components/layout/navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+import { Link, useLocation } from 'react-router-dom'
+import './Navbar.css'
+
+const Navbar = () => {
+	const links = [
+		{
+			name: 'Dashboard',
+            path: '/',
+		},
+		{
+			name: 'Scouts',
+            path: '/scouts',
+		},
+		{
+			name: 'Ranking',
+            path: '/rankings',
+		}
+	];
+
+	const location = useLocation();
+
+	return (
+		<div className='navbar'>
+			<div className='logo'>
+				<Link to="/">
+					futz
+				</Link>
+			</div>
+			<ul>
+				{links.map(link =>(
+					<Link to={link.path} className={location.pathname === link.path ? "active" : ""} key={link.name}><li>{link.name}</li></Link>
+				))}
+			</ul>
+		</div>
+	)
+}
+
+export default Navbar
