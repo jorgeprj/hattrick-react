414faf8434e831708766ef80525368cdc6fd7a71
jorgeprj <jlmp2002@gmail.com>
2023-11-19 19:12:39 -0300

    feat: loading

---
+++ src/components/layout/loading/Loading.jsx
@@ -0,0 +1,14 @@
+import './Loading.css'
+
+const Loading = () => {
+  return (
+    <div className='loading'>
+        <div>
+            <h2>futz</h2>
+        </div>
+        <span className="loader"></span>
+    </div>
+  )
+}
+
+export default Loading
