d7d813d28651680d12a44fa5bd3b03809c907781
jorgeprj <jlmp2002@gmail.com>
2023-11-20 17:24:32 -0300

    feat: available to pre-contract

--- src/components/playerLayout/playerHeader/PlayerHeader.jsx
+++ src/components/playerLayout/playerHeader/PlayerHeader.jsx
@@ -1,35 +1,40 @@
-import { calculateFutzScore } from '../../../utils/calculateFutzScoreUtils';
 import { formatCurrency } from '../../../utils/formatCurrencyUtils';
 import { getOverallColor } from '../../../utils/overallColorUtils';
 import './PlayerHeader.css';
 
-const PlayerHeader = ( {player, year} ) => {
-  return (
-    <div className='player-header'>
-        <div className='header-info'>
-        <div className={`player-overall ${getOverallColor(player.overall)}`}>
-                <h4>Overall</h4>
-                <p>{player.overall}</p>
-            </div>
-            <div className='player-value'>
-                <h4>Value</h4>
-                <p>{formatCurrency(player.value)}</p>
-            </div>
-            <div className='player-wage'>
-                <h4>Wage</h4>
-                <p>{formatCurrency(player.wage)}</p>
-            </div>
-            <div className='player-release-clause'>
-                <h4>Release clause</h4>
-                <p>{formatCurrency(player.releaseClause)}</p>
-            </div>
-            <div className='player-contract'>
-                <h4>Contract until</h4>
-                <p>{player.contract}</p>
+const PlayerHeader = ({ player, year }) => {
+
+    return (
+        <div className='player-header'>
+            <div className='header-info'>
+                <div className={`player-overall ${getOverallColor(player.overall)}`}>
+                    <h4>Overall</h4>
+                    <p>{player.overall}</p>
+                </div>
+                <div className='player-value'>
+                    <h4>Value</h4>
+                    <p>{formatCurrency(player.value)}</p>
+                </div>
+                <div className='player-wage'>
+                    <h4>Wage</h4>
+                    <p>{formatCurrency(player.wage)}</p>
+                </div>
+                <div className='player-release-clause'>
+                    <h4>Release clause</h4>
+                    <p>{formatCurrency(player.releaseClause)}</p>
+                </div>
+                <div className='player-contract'>
+                    <h4>Contract until</h4>
+                    {player.contract === (year + 1) ? (
+                        <p className='available'>{player.contract}</p>
+                    ) : (
+                        <p>{player.contract}</p>
+                    )}
+                </div>
+
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default PlayerHeader
