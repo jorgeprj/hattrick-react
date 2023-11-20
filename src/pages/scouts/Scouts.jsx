3f5ad610f4f187b270a518e33e25b94db94127d3
jorgeprj <jlmp2002@gmail.com>
2023-11-20 17:55:14 -0300

    feat: loading screen

--- src/pages/scouts/Scouts.jsx
+++ src/pages/scouts/Scouts.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from 'react';
 import ScoutList from '../../components/scoutList/ScoutList'
+import Loading from '../../components/layout/loading/Loading';
+
 import './Scouts.css'
 
 const Scouts = ({ players, setPlayers, year }) => {
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		const delay = 1000;
+	
+		const timeoutId = setTimeout(() => {
+		  setIsLoading(false);
+		}, delay);
+	
+		return () => clearTimeout(timeoutId);
+	  }, []);
+	
+	  if (isLoading) {
+		return <Loading />;
+	  }
+
 	return (
 		<div className='scouts'>
 			<section>
