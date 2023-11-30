94246cdcb24821b63f951cb1e7a41f9526890009
jorgeprj <jlmp2002@gmail.com>
2023-11-29 21:29:04 -0300

    feat: dashboard header

---
+++ src/components/dashboardLayout/dashboardHeader/DashboardHeader.jsx
@@ -0,0 +1,39 @@
+import './DashboardHeader.css'
+
+const DashboardHeader = ({ section, setSection }) => {
+
+    const sections = ['overview', 'squad', 'table'];
+
+    const handleClick = (section) => {
+        setSection(section);
+    };
+
+    return (
+        <div className='dashboard-header'>
+            <header>
+                <div className='team-photo'>
+                    <img src="./src/assets/teams/113926.png" alt="Team Logo" />
+                </div>
+                <div className='team-basic-info'>
+                    <h3>Salford City</h3>
+                    <div className='country'>
+                        <img src="./src/assets/flags/england.png" alt="England" />
+                        <h4>England</h4>
+                    </div>
+
+                </div>
+            </header>
+            <nav>
+                <ul>
+                    {sections.map((s) => (
+                        <li key={s} className={section === s ? 'active' : ''} onClick={() => handleClick(s)}>
+                            {s.charAt(0).toUpperCase() + s.slice(1)}
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default DashboardHeader
