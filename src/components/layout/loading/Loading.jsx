3b4bea25807e81ae4ad78c96e90c2029e3d59c07
jorgeprj <jlmp2002@gmail.com>
2023-12-03 00:24:41 -0300

    refactor: loading and star rating

--- src/components/layout/loading/Loading.jsx
+++ src/components/layout/loading/Loading.jsx
@@ -1,14 +1,14 @@
 import './Loading.css'
 
 const Loading = () => {
-  return (
-    <div className='loading'>
-        <div>
-            <h2>futz</h2>
+    return (
+        <div className='loading'>
+            <div>
+                <h2>futz</h2>
+            </div>
+            <span className="loader"></span>
         </div>
-        <span className="loader"></span>
-    </div>
-  )
+    )
 }
 
 export default Loading
