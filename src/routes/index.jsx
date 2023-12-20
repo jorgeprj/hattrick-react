692c39b7e5211aabdbbdee67ba79923e96f52fc5
jorgeprj <jlmp2002@gmail.com>
2023-12-20 17:55:49 -0300

    chore: db , assets and routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,6 +3,7 @@ import Home from '../pages/home/Home'
 import NotFound from '../pages/notFound/NotFound'
 import PlayerReport from '../pages/playerReport/PlayerReport'
 import Player from '../pages/player/Player'
+import Coach from '../pages/coach/Coach'
 
 export const AppRoutes = ({year}) => {
 
@@ -10,6 +11,7 @@ export const AppRoutes = ({year}) => {
         <Routes>
             <Route path="/" element={<Navigate to="/home"/>}/>
             <Route path="/home" element={<Home year={year} />}/>
+            <Route path="/coach" element={<Coach year={year} />}/>
             <Route path="/player/:id" element={<Player year={year} />}/>
             <Route path="/player/report/:id" element={<PlayerReport year={year} />}/>
             <Route path="*" element={<NotFound />}/>
