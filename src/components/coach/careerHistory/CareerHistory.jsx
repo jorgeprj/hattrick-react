894028a03fd0226c741bda30bf07b81131ec39d0
jorgeprj <jlmp2002@gmail.com>
2023-12-20 17:55:16 -0300

    feat: coach page

---
+++ src/components/coach/careerHistory/CareerHistory.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import './CareerHistory.css'
+
+import { useState } from 'react';
+import { FaAngleDown, FaAngleUp } from "react-icons/fa6"
+import TeamTag from '../../shared/teamTag/TeamTag';
+
+
+const CareerHistory = ({ coach }) => {
+    const [showAllTeams, setShowAllTeams] = useState(false);
+    const teamsToDisplay = showAllTeams ? coach.careerHistory : coach.careerHistory.slice(0, 3);
+
+    return (
+        <section className='career-history'>
+            <h4>Career History</h4>
+            <div className='teams'>
+                {teamsToDisplay.map((team, index) => (
+                    <div key={index} className='coach-team'>
+                        <TeamTag path={`../../src/assets/teams/${team.team.id}.png`}/>
+                        <div className='team-text'>
+                            <h5>{team.team.name}</h5>
+                            <h6>{team.position} ({team.season})</h6>
+                        </div>
+                    </div>
+                ))}
+                {coach.careerHistory.length > 2 && !showAllTeams && (
+                    <div className='button'>
+                        <button className="show-older-button" onClick={() => setShowAllTeams(true)}>
+                            <FaAngleDown />
+                            Show Older
+                        </button>
+                    </div>
+                )}
+                {showAllTeams && (
+                    <div className='button'>
+                        <button className="collapse-button" onClick={() => setShowAllTeams(false)}>
+                            <FaAngleUp />
+                        </button>
+                    </div>
+                )}
+            </div>
+        </section>
+    )
+}
+
+export default CareerHistory
---
+++ src/pages/coach/Coach.jsx
@@ -0,0 +1,116 @@
+import React, { useEffect, useState } from 'react'
+import './Coach.css'
+import { getCoach } from '../../services/coach/coachService';
+import Loading from '../../components/layout/loading/Loading';
+import CareerHistory from '../../components/coach/careerHistory/CareerHistory';
+import Footer from '../../components/layout/footer/Footer';
+import PlayerHead from '../../components/shared/playerHead/PlayerHead';
+
+
+const Coach = ({year}) => {
+    const [isLoading, setIsLoading] = useState(true);
+    const [coach, setCoach] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const coachData = await getCoach();
+                setCoach(coachData);
+                setIsLoading(false);
+            } catch (error) {
+                console.error('Error fetching coach:', error);
+            }
+        };
+
+        fetchData();
+    });
+
+    if (isLoading) {
+        return <Loading />;
+    }
+
+    return (
+        <div className='coach'>
+            <h1>
+                {coach.name}
+            </h1>
+            <section className='content'>
+                <section className='column-1'>
+                    <section className='header'>
+                        <div className='coach-image'>
+                            <img className='flag' src={`../../src/assets/flags/${coach.nationality}.png`} alt={`Flag of ${coach.nationality}`} />
+                            <PlayerHead playerId={coach.id}/>
+                        </div>
+                    </section>
+                    <section className='coach-infos'>
+                        <section className='other-infos'>
+                            <CareerHistory coach={coach} />
+                        </section>
+                    </section>
+                </section>
+
+                <section className='column-2'>
+                    <div className='title'>
+                        <h2>Detailed sheet</h2>
+                        <h4>|</h4>
+                        <p>Discussion</p>
+                    </div>
+                    <section className='coach-content'>
+                        <section>
+                            <h3>Biography</h3>
+                            <p>{coach.bio}</p>
+                        </section>
+                        {coach.titles.length > 0 && (
+                            <section>
+                                <h3>Coach Titles</h3>
+                                <div className='awards'>
+                                    {coach.titles.map(title => (
+                                        <p className='award' key={`${title.name} ${title.season}`}>
+                                            <img className="team-image" src={`../src/assets/teams/${title.team}.png`} alt="Team logo" />
+                                            {title.name}
+                                            : {""}
+                                            {title.season}
+                                        </p>
+                                    ))}
+                                </div>
+                            </section>
+                        )}
+                    </section>
+                </section>
+
+                <section className='column-3'>
+                    <section className='column-3'>
+                        {coach.stats.length > 0 && coach.stats[0].season === year - 1 && (
+                            <section className='coach-stats'>
+                                <h4>Last Season Stats</h4>
+                                <div className='stats'>
+                                    <p>Matches: <strong>{coach.stats[0].wins + coach.stats[0].losses + coach.stats[0].draws}</strong></p>
+                                    <p>Wins: <strong>{coach.stats[0].wins}</strong></p>
+                                    <p>Draws: <strong>{coach.stats[0].draws}</strong></p>
+                                    <p>Losses: <strong>{coach.stats[0].losses}</strong></p>
+                                    <p>Win rate: <strong>{(coach.stats[0].wins*3/((coach.stats[0].wins + coach.stats[0].losses + coach.stats[0].draws)*3)*100).toFixed(1)} %</strong></p>
+                                </div>
+                            </section>
+                        )}
+
+                        <section>
+                            <h4>Author</h4>
+                            <div className='logo'>hattrick</div>
+                        </section>
+                        <section>
+                            <h4>Sources</h4>
+                            <div className='links'>
+                                <a href="https://www.wikipedia.org">https://www.wikipedia.org</a>
+                                <a href="https://www.transfermarkt.com">https://www.transfermarkt.com</a>
+                            </div>
+                        </section>
+                    </section>
+                </section>
+
+            </section>
+            <Footer />
+        </div>
+    )
+}
+
+export default Coach
