b6a4076bf262e303617cebc76c5eede20928990c
jorgeprj <jlmp2002@gmail.com>
2023-12-19 22:25:39 -0300

    fix: components
    -routes
    -footer
    -loading
    -navbar

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,22 +1,18 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
+import Home from '../pages/home/Home'
+import NotFound from '../pages/notFound/NotFound'
+import PlayerReport from '../pages/playerReport/PlayerReport'
+import Player from '../pages/player/Player'
 
-import Dashboard from '../pages/dashboard/Dashboard'
-import Scouts from '../pages/scouts/Scouts'
-import ScoutPlayer from '../pages/scoutPlayer/ScoutPlayer';
-import Player from '../pages/player/Player';
-import Team from '../pages/team/Team';
-
-export const AppRoutes = ( {year} ) => {
+export const AppRoutes = ({year}) => {
 
     return (
         <Routes>
-            <Route path="/" element={<Navigate to="/dashboard"/>}/>
-            <Route path="/dashboard" element={<Dashboard year={year}/>}/>
-            <Route path="/scouts" element={<Scouts year={year}/>}/>
-            <Route path="/player/scout/:id" element={<ScoutPlayer year={year}/>}/>
-            <Route path="/player/:id" element={<Player year={year}/>}/>
-            <Route path="/team/:id" element={<Team/>}/>
-            <Route path="*" element={<Navigate to="/"/>}/>
+            <Route path="/" element={<Navigate to="/home"/>}/>
+            <Route path="/home" element={<Home year={year} />}/>
+            <Route path="/player/:id" element={<Player year={year} />}/>
+            <Route path="/player/report/:id" element={<PlayerReport year={year} />}/>
+            <Route path="*" element={<NotFound />}/>
         </Routes>
     )
 }
