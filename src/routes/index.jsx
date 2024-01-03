3741d95cfcc1a64218a1e9a8260b226d42c5242b
jorgeprj <jlmp2002@gmail.com>
2024-01-03 17:32:05 -0300

    feat: scouts page

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,11 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from '../pages/home/Home'
 import NotFound from '../pages/notFound/NotFound'
-import Player from '../pages/player/Player'
-import Coach from '../pages/coach/Coach'
-import Team from '../pages/team/Team'
+import Player from '../pages/profile/player/Player'
+import Coach from '../pages/profile/coach/Coach'
+import Team from '../pages/profile/team/Team'
+import Edit from '../pages/edit/Edit'
+import Scouts from '../pages/scouts/Scouts'
 
 export const AppRoutes = ({year}) => {
 
@@ -14,6 +16,8 @@ export const AppRoutes = ({year}) => {
             <Route path="/coach/:id" element={<Coach year={year} />}/>
             <Route path="/player/:id" element={<Player year={year} />}/>
             <Route path="/team/:id" element={<Team year={year} />}/>
+            <Route path="/scouts" element={<Scouts year={year} />}/>
+            <Route path="/admin/edit" element={<Edit/>}/>
             <Route path="*" element={<NotFound />}/>
         </Routes>
     )
