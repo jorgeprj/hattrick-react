52a6c352011415aef6e768851d5903322b12fbc2
jorgeprj <jlmp2002@gmail.com>
2024-01-04 15:54:27 -0300

    feat: players crud

--- src/services/players/playersService.js
+++ src/services/players/playersService.js
@@ -35,3 +35,47 @@ export const getPlayer = async (playerId) => {
         throw error;
     }
 }
+
+export const editPlayer = async (playerId, updatedPlayerData) => {
+    try {
+        const response = await fetch(`${API_BASE_URL}/players/${playerId}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(updatedPlayerData),
+        });
+
+        if (!response.ok) {
+            throw new Error('Error editing player information');
+        }
+
+        const editedPlayerData = await response.json();
+
+        return editedPlayerData;
+
+    } catch (error) {
+        console.error('Error during API request:', error);
+        throw error;
+    }
+};
+
+export const deletePlayer = async (playerId) => {
+    try {
+        const response = await fetch(`${API_BASE_URL}/players/${playerId}`, {
+            method: 'DELETE',
+        });
+
+        if (!response.ok) {
+            throw new Error('Error deleting player');
+        }
+
+        const deletedPlayerData = await response.json();
+
+        return deletedPlayerData;
+
+    } catch (error) {
+        console.error('Error during API request:', error);
+        throw error;
+    }
+};
